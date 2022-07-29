08de741f0f43c27a71b2c52127718f076581be41
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-07-28 22:14:27 -0700

    1st C#

---
+++ C#/salary_bonus.cs
@@ -0,0 +1,16 @@
+public static class Kata
+    {
+        public static string bonus_time(int salary, bool bonus)
+        {
+         
+          
+    if (bonus) 
+  {
+  return("$" + (salary * 10));
+  } else{
+         return  "$" + salary;
+    }
+
+         
+        }
+    }
