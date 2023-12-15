1b2460d792646601431c4ba9bf12da05c593b728
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-12-15 18:29:18 -0500

    remove ! from end of string

---
+++ Java/duckDuckGoose.java
@@ -0,0 +1,13 @@
+public class duckDuckGoose {
+    Player[] players = makePlayerArr(new String[] {"a", "b", "c", "d", "c", "e", "f", "g", "h", "z"});
+    public static double arrIndexFind(Player[] players, int goose) {
+     return players
+        
+    }
+
+    public static void main(String[] args) {
+      
+     
+        System.out.println(im him);
+    }
+}
