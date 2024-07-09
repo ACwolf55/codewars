661702b892d9527741dda0612bdbae79ac6c11fc
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2024-07-09 17:24:34 -0400

    short long short

---
+++ Java/ShortLongShort.java
@@ -0,0 +1,11 @@
+public class public class ShortLongShort {
+    public static String solution(String a, String b) {
+     StringBuilder comboString = new StringBuilder();
+      if(a.length()>b.length()){
+        comboString.append(b).append(a).append(b);
+      }else{
+      comboString.append(a).append(b).append(a);
+      }
+        return comboString.toString();
+    }
+}
