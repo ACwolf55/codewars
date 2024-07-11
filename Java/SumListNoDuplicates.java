97e7adc0d38f065138c255a52d620abdbadd4c2c
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2024-07-11 11:38:39 -0400

    sumlistnoduplicates

---
+++ Java/SumListNoDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Solution{
+  
+    public static int sumNoDuplicates(int[] arr){
+     Map<Integer, Integer> countMap = new HashMap<>();
+  int sum = 0 ;
+  for( int number : arr){
+    countMap.put(number, countMap.getOrDefault(number,0)+1);
+  }
+
+  for(Map.Entry<Integer,Integer> entry : countMap.entrySet() ){
+    if(entry.getValue()==1){
+      sum += entry.getKey();
+    }
+  }
+
+  return sum;
+    }
+  
+}
