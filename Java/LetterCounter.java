6e32d0783172c570f4f83912080cbc3a401defdb
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2025-02-27 13:45:14 -0500

    LetterCounter

---
+++ Java/LetterCounter.java
@@ -0,0 +1,11 @@
+public class LetterCounter {
+    public static int strCount(String str, char letter) {
+      int count = 0;
+      for(char c:str.toCharArray()){
+        if(c==letter){
+          count++;
+        }
+  }
+  return count;
+    }
+  }
